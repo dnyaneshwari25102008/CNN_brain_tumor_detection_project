16c6e2c0b4f5c4e62c31eeed8ed912e872092e8c
dnyaneshwari25102008 <bhosalednyaneshwari36@gmail.com>
2026-07-21 17:54:26 +0530

    Update xyz.py

--- xyz.py
+++ xyz.py
@@ -2,8 +2,10 @@ import streamlit
 import numpy
 import PIL
 import tensorflow
+import pandas
 
 print("Streamlit:", streamlit.__version__)
 print("NumPy:", numpy.__version__)
 print("Pillow:", PIL.__version__)
 print("TensorFlow:", tensorflow.__version__)
+print("pandas:",pandas.__version__)
