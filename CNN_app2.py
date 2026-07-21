bcb0c716eab24f897aca9e9c419ea707b63fe716
dnyaneshwari25102008 <bhosalednyaneshwari36@gmail.com>
2026-07-21 23:13:22 +0530

    Update CNN_app2.py

--- CNN_app2.py
+++ CNN_app2.py
@@ -65,6 +65,6 @@ if uploaded_file is not None:
     prob = prediction[0][0]
 
     if prob > 0.5:
-        st.success("Yes, you have a brain tumor")
+        st.success("NO, you have a brain tumor")
     else:
-        st.success("No, you don't have a brain tumor")
+        st.success("YES, you don't have a brain tumor")
